cb482a38281c1b1311eb4fd5e308836624cfac09
pravedens <107057525+pravedens@users.noreply.github.com>
2024-04-06 18:52:22 +0500

    Установка Laravel, vue, vue-router, tailwind

---
+++ resources/js/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1 class="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+        <router-link to="index">Index</router-link>
+        <router-link to="page">Page</router-link>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App"
+}
+</script>
+
+<style scoped>
+
+</style>
