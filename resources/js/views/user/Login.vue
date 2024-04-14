dfa25cef1b7cd58b0d332e1dd6b6d46a6d013a6a
pravedens <107057525+pravedens@users.noreply.github.com>
2024-04-14 11:43:55 +0500

    Подключение Sanctum

---
+++ resources/js/views/user/Login.vue
@@ -0,0 +1,37 @@
+<template>
+<div class="w-25">
+    <input v-model="email" type="email" placeholder="email" class="form-control mt-3 mb-3">
+    <input v-model="password" type="password" placeholder="password" class="form-control mb-3">
+    <input @click.prevent="login" type="submit" value="login" class="btn btn-primary">
+</div>
+</template>
+
+<script>
+
+export default {
+    name: "Login",
+
+    data() {
+        return {
+            email: null,
+            password: null,
+        }
+    },
+
+    methods: {
+        login() {
+            axios.get('/sanctum/csrf-cookie').then(response => {
+                axios.post('/login', { email: this.email, password: this.password })
+                    .then( r => {
+                        localStorage.setItem('x_xsrf_token', r.config.headers['X-XSRF-TOKEN'])
+                        this.$router.push({name: 'user.personal'})
+                    })
+            });
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
